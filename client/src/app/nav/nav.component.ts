e6b808e1155847c6ebbdc3810d56063081a8eb60
Roman <romeiro2689@gmail.com>
2021-08-12 21:37:19 +0300

    section 6 done

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AccountService} from '../_services/account.service';
 import {Observable} from 'rxjs';
 import {User} from '../_models/user';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,21 +15,26 @@ export class NavComponent implements OnInit {
   public model: any = {};
 
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
 
   public login(): void {
     this.accountService.login((this.model)).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   public logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AccountService} from '../_services/account.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,10 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +24,8 @@ export class RegisterComponent implements OnInit {
   register() {
     this.accountService.register(this.model).subscribe(response => {
       this.cancel();
+    }, error => {
+      this.toastr.error(error.error);
     });
   }
 
