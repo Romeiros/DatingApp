ab09b78ff8b76294d9712a05ee13eb44447e5792
Roman <romeiro2689@gmail.com>
2021-11-11 19:46:32 +0300

    section 17 done

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -3,6 +3,11 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {getPaginatedResult, getPaginationHeaders} from './paginationHelper';
 import {Message} from '../_models/message';
+import {HubConnection, HubConnectionBuilder} from '@microsoft/signalr';
+import {User} from '../_models/user';
+import {BehaviorSubject} from 'rxjs';
+import {take} from 'rxjs/operators';
+import {Group} from '../_models/group';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +15,57 @@ import {Message} from '../_models/message';
 export class MessageService {
 
   private baseUrl = environment.apiUrl;
+  hubUrl = environment.hubUrl;
+  private hubConnection: HubConnection;
+  private messageThreadSource = new BehaviorSubject<Message[]>([]);
+  messageThread$ = this.messageThreadSource.asObservable();
 
   constructor(private http: HttpClient) { }
+
+  createHubConnection(user: User, otherUserName: string) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'message?user=' + otherUserName, {
+        accessTokenFactory: () => user.token
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection
+      .start()
+      .catch(error => console.log(error));
+
+    this.hubConnection.on('ReceiveMessageThread', messages => {
+      this.messageThreadSource.next(messages);
+    });
+
+    this.hubConnection.on('NewMessage', message => {
+      this.messageThread$.pipe(take(1)).subscribe(messages => {
+        this.messageThreadSource.next([...messages, message]);
+      });
+    });
+
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if (group.connections.some(x => x.username === otherUserName)) {
+        this.messageThread$.pipe(take(1)).subscribe(messages => {
+          messages.forEach(message => {
+            if (!message.dateRead) {
+              message.dateRead = new Date(Date.now());
+            }
+          });
+          this.messageThreadSource.next([...messages]);
+        });
+      }
+    });
+  }
+
+  stopHubConnection() {
+    if (this.hubConnection) {
+      this.hubConnection
+        .stop()
+        .catch(error => console.log(error));
+    }
+
+  }
 
   getMessages(pageNumber, pageSize, container) {
     let params = getPaginationHeaders(pageNumber, pageSize);
@@ -23,8 +77,9 @@ export class MessageService {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content});
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection.invoke('SendMessage', {recipientUsername: username, content})
+      .catch(error => console.log(error));
   }
 
   deleteMessage(id: number) {
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,20 +1,39 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
 import {Member} from '../../_models/member';
 import {MembersService} from '../../_services/members.service';
 import {ToastrService} from 'ngx-toastr';
+import {PresenceService} from '../../_services/presence.service';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.scss']
+  styleUrls: ['./member-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class MemberCardComponent implements OnInit {
 
   @Input() member: Member;
+  isOnline$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor(private membersService: MembersService, private toastr: ToastrService) { }
+  constructor(
+    private membersService: MembersService,
+    private toastr: ToastrService,
+    public presenceService: PresenceService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    this.presenceService.onlineUsers$.subscribe(onlineUsers => {
+      console.log(onlineUsers);
+      if (onlineUsers.includes(this.member.userName)) {
+        this.isOnline$.next(true);
+      } else {
+        this.isOnline$.next(false);
+      }
+      this.cdr.detectChanges();
+      console.log(this.isOnline$.value)
+    })
   }
 
   addLike(member: Member) {
