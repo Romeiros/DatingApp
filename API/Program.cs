c33859e783e7fbe82a8782df29e95b2769376e55
Roman <romeiro2689@gmail.com>
2021-11-18 21:32:05 +0300

    section 19

--- API/Program.cs
+++ API/Program.cs
@@ -35,7 +35,7 @@ namespace API
                 logger.LogError(ex, "An error occured during migration");
             }
 
-            await host.RunAsync();
+            await host.RunAsync(); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
