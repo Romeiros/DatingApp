6b57a0a24eb99c233cd20d93b578db80cc130dc8
Roman <romeiro2689@gmail.com>
2021-11-19 22:01:38 +0300

    ssl add

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -37,7 +37,7 @@ export class RegisterComponent implements OnInit {
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]],
     });
   }
